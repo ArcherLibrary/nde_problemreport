00a41e797556a5fd2f2338989b4b6ae463a6f248
caitlinjb <111923784+caitlinjb@users.noreply.github.com>
2025-12-17 09:20:00 -0600

    Update customComponentMappings.ts

--- customComponentMappings.ts
+++ customComponentMappings.ts
@@ -1,6 +1,6 @@
-import { NdeProblemReportCustomComponent } from '../nde-problem-report-custom/nde-problem-report-custom.component';
+import { NdeProblemReportCustom } from '../nde-problem-report-custom/nde-problem-report-custom.component';
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
-	['nde-full-display-service-container-before', NdeProblemReportCustomComponent], 
+	['nde-full-display-service-container-before', NdeProblemReportCustom], 
 ]);
